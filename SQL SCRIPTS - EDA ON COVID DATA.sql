04e27ff92c7faedc1e6417e1d067f8cb5c6b9e0e
ziaurrahmanUK <ziaurrahmanUK@users.noreply.github.com>
2024-09-24 14:38:20 +0100

    Update SQL SCRIPTS - EDA ON COVID DATA.sql

--- SQL SCRIPTS - EDA ON COVID DATA.sql
+++ SQL SCRIPTS - EDA ON COVID DATA.sql
@@ -1,6 +1,6 @@
 -- 1. 
 
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+Select SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
 where continent is not null 
@@ -24,8 +24,8 @@ order by 1,2
 -- We take these out as they are not inluded in the above queries and want to stay consistent
 -- European Union is part of Europe
 
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+Select location, SUM(new_deaths) as TotalDeathCount
+From CovidDeaths
 --Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
@@ -36,7 +36,7 @@ order by TotalDeathCount desc
 -- 3.
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
@@ -46,7 +46,7 @@ order by PercentPopulationInfected desc
 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 --Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
